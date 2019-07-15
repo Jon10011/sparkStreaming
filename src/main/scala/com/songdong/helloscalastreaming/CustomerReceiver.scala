4865dc47beeca8a5e756a5a83237fb517cc80ee5
Jon <dong_song1@163.com>
2019-07-16 00:32:08 +0800

    sparkStreaming 使用自定义接收器获取数据

---
+++ src/main/scala/com/songdong/helloscalastreaming/CustomerReceiver.scala
@@ -0,0 +1,57 @@
+package com.songdong.helloscalastreaming
+
+import java.io.{BufferedReader, InputStreamReader}
+import java.net.Socket
+
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.receiver.Receiver
+
+class CustomerReceiver(host: String, port: Int) extends Receiver[String](StorageLevel.MEMORY_ONLY) {
+  /**
+    * 自定义接收器
+    */
+  //接收器启动时调用
+  override def onStart(): Unit = {
+    new Thread("receiver") {
+      override def run(): Unit = {
+        //接收数据并提交给框架
+        receive()
+      }
+    }.start()
+  }
+
+  def receive() = {
+    var socket: Socket = null
+    var input: String = null
+    try {
+      socket = new Socket(host, port)
+
+      //生成输入流
+      val reader = new BufferedReader(new InputStreamReader(socket.getInputStream))
+
+      //接收数据
+      //      input = reader.readLine()
+      //      store(input)
+
+      //循环接收数据
+      while (!isStopped() && (input = reader.readLine()) != null) {
+        store(input)
+      }
+      //另一种写法
+      //      while (!isStopped() && input != null){
+      //        store(input)
+      //        input = reader.readLine()
+      //      }
+      //重启
+      restart("restart")
+    } catch {
+      case e: java.net.ConnectException => restart("restart")
+      case t: Throwable => restart("restart")
+    }
+  }
+
+  //接收器关闭时调用
+  override def onStop(): Unit = {
+
+  }
+}
--- src/main/scala/com/songdong/helloscalastreaming/WordCound.scala
+++ src/main/scala/com/songdong/helloscalastreaming/WordCound.scala
@@ -11,8 +11,11 @@ object WordCound {
     val ssc = new StreamingContext(conf,Seconds(5))
 
     //获取数据,监控92.168.1.111端口9999获取数据
-    val lineDStream = ssc.socketTextStream("192.168.1.111",9999)
+//    val lineDStream = ssc.socketTextStream("192.168.1.111",9999)
 
+    //使用自定义接收器获取数据
+
+    val lineDStream = ssc.receiverStream(new CustomerReceiver("192.168.1.111",9999))
     //DStream[String]
     val wordsDStream = lineDStream.flatMap(_.split(" "))
 
