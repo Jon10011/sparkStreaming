c8c9014287c6ea3272393ec37df00c2ba1e71689
Jon <dong_song1@163.com>
2019-07-16 21:42:18 +0800

    sparkStreaming 有状态的转换

--- src/main/scala/com/songdong/helloscalastreaming/WordCound.scala
+++ src/main/scala/com/songdong/helloscalastreaming/WordCound.scala
@@ -8,22 +8,33 @@ object WordCound {
     //conf
     val conf = new SparkConf().setAppName("wc").setMaster("local[*]")
 
-    val ssc = new StreamingContext(conf,Seconds(5))
+    val ssc = new StreamingContext(conf, Seconds(5))
+
+    ssc.checkpoint("./check")
 
     //获取数据,监控92.168.1.111端口9999获取数据
-//    val lineDStream = ssc.socketTextStream("192.168.1.111",9999)
+    //    val lineDStream = ssc.socketTextStream("192.168.1.111",9999)
 
     //使用自定义接收器获取数据
 
-    val lineDStream = ssc.receiverStream(new CustomerReceiver("192.168.1.111",9999))
+    val lineDStream = ssc.receiverStream(new CustomerReceiver("192.168.1.111", 9999))
     //DStream[String]
     val wordsDStream = lineDStream.flatMap(_.split(" "))
 
     //DStream[(String,1)]
-    val k2vDStream = wordsDStream.map((_,1))
+    val k2vDStream = wordsDStream.map((_, 1))
 
     //DStream[(String,sum)]
-    val result = k2vDStream.reduceByKey(_+_)
+    //    val result = k2vDStream.reduceByKey(_+_)
+
+    //保存上次的状态信息--》有状态的转换
+    val updateFuc = (v: Seq[Int], state: Option[Int]) => {
+
+      val preStatus = state.getOrElse(0)
+      Some(preStatus + v.sum)
+    }
+    val result = k2vDStream.updateStateByKey(updateFuc)
+
 
     result.print()
 
