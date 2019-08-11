0a2a7d959824e52a336e74a3b0aac3f0cc65e232
Jon <dong_song1@163.com>
2019-08-12 02:31:39 +0800

    sparking socket通信与保存数据到外部数据库

--- src/main/scala/com/songdong/test/testStreaming.scala
+++ src/main/scala/com/songdong/test/testStreaming.scala
@@ -1,6 +1,9 @@
 package com.songdong.test
 
+import java.sql.DriverManager
+
 import org.apache.spark.SparkConf
+import org.apache.spark.deploy.DeployMessages.DriverStateChanged
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object testStreaming {
@@ -13,6 +16,24 @@ object testStreaming {
     val lines = ssc.socketTextStream("192.168.1.111",9999)
 
     val words = lines.flatMap(_.split(" ")).map(word => (word,1)).reduceByKey(_ + _)
+
+
+    //保存数据到外部数据库
+
+    words.foreachRDD(rdd => rdd.foreachPartition(line =>{
+      //加载driver 拓展可以自定义数据库连接池
+      Class.forName("com.mysql.jdbc.Driver")
+      val conn = DriverManager
+        .getConnection("jdbc:mysql://192.168.1.111:3306/test","root","songdong123")
+      try{
+        for(row <- line){
+          val sql = "insert into webCount(titleName,count) values('"+row._1+"',"+row._2+")"
+          conn.prepareStatement(sql).executeLargeUpdate()
+        }
+      }finally {
+        conn.close()
+      }
+    }))
 
     words.print()
     ssc.start()
