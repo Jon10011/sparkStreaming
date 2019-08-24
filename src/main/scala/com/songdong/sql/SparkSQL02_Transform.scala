d50f95bc02e4b1f26950310708c3a54ea579eee1
Jon <dong_song1@163.com>
2019-08-25 02:08:55 +0800

    sparkSQL转换操作

--- src/main/scala/com/songdong/sql/SparkSQL02_Transform.scala
+++ src/main/scala/com/songdong/sql/SparkSQL02_Transform.scala
@@ -21,22 +21,36 @@ object SparkSQL02_Transform {
     //创建RDD
     val rdd: RDD[(Int, String, Int)] = spark.sparkContext.makeRDD(List((1, "zhangsan", 30), (2, "lisi", 40), (3, "wangwu", 50)))
 
-    //转换DF
+    //RDD转换DF
     val df: DataFrame = rdd.toDF("id", "name", "age")
 
-    //转换DS
+    //DF转换DS
     val ds: Dataset[User] = df.as[User]
 
-    //转换为DF
+    //DS转换为DF
     val df2: DataFrame = ds.toDF()
 
-    //转换为RDD
+    //DF转换为RDD
     val rdd2: RDD[Row] = df2.rdd
 
     rdd2.foreach(row =>{
       //获取数据时，可以通过索引获取
       println(row.get(1))
     })
+
+    //RDD转换DS
+
+    val userRDD: RDD[User] = rdd.map {
+      case (id, name, age) => {
+        User(id, name, age)
+      }
+    }
+    val userDS: Dataset[User] = userRDD.toDS()
+    val rdd3: RDD[User] = userDS.rdd
+    rdd3.foreach(
+      //可以通过类的属性去访问
+      row=>println(row.age)
+    )
 
     spark.stop()
 
