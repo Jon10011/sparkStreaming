043c71364cce8b8a78cf3629c2b61e0dbcf64d2e
Jon <dong_song1@163.com>
2019-09-01 15:58:37 +0800

    spark创建RDD的方式

--- src/main/scala/com/songdong/rdd/spark01.scala
+++ src/main/scala/com/songdong/rdd/spark01.scala
@@ -11,19 +11,19 @@ object spark01 {
 
     //从内存创建makeRDD,底层实现就是parallelize,默认分区取核数与2的最大值
     val listRDD: RDD[Int] = sc.makeRDD(List(1,2,3,4))
-//    val listRDD1: RDD[Int] = sc.makeRDD(List(1,2,3,4),2)
+    //    val listRDD1: RDD[Int] = sc.makeRDD(List(1,2,3,4),2)
 
-//    //从内存中创建parallelize,并行
+    //    //从内存中创建parallelize,并行
     val arrayRDD: RDD[Int] = sc.parallelize(Array(1,2,3,4))
-//
-//    //从外部存储创建，默认分区取核数与2的最小值
-//    //默认情况下可以读取项目路径。也可以读取其他路径：：hdfs
-//    //默认文件中读取的数据都是字符串类型
+    //
+    //    //从外部存储创建，默认分区取核数与2的最小值
+    //    //默认情况下可以读取项目路径。也可以读取其他路径：：hdfs
+    //    //默认文件中读取的数据都是字符串类型
     //读取文件时传递的分区参数为最小分区数，但是不一定是这个分区数，取决于hadoop读取文件时的分片规则
     val fileRDD: RDD[String] = sc.textFile("in")
-//    val fileRDD1: RDD[String] = sc.textFile("in",3)
+    //    val fileRDD1: RDD[String] = sc.textFile("in",3)
 
-//    listRDD.collect().foreach(println(_))
+    //    listRDD.collect().foreach(println(_))
 
     listRDD.saveAsTextFile("output")
   }
